b545fcc8d69ba673645dbf75bf432713054c202f
Hikkun098 <mhhkn098@gmail.com>
2026-06-21 01:21:58 +0900

    02_constants

--- 02_constants/constants.go
+++ 02_constants/constants.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello from", "constants")
+	const consumption_Tax_Rate float64 = 0.1
+	const name string = "Murakami"
+
+	fmt.Println("消費税率:", consumption_Tax_Rate)
+	fmt.Println("名前:", name)
 }
