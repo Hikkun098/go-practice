0ffeaea6f042f17e6d96e84d5da2d2d68c1eba77
Hikkun098 <mhhkn098@gmail.com>
2026-06-23 23:43:58 +0900

    03_types: 型について少し練習した

--- 03_types/types.go
+++ 03_types/types.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello from", "types")
+	var message string = "Thank you!"
+	var level int = 100
+	var point float64 = 4.5
+	var isEngineer bool = true
+
+	fmt.Printf("メッセージ:%s", message)
+	fmt.Printf("レベル:%d", level)
+	fmt.Printf("ポイント:%f", point)
+	fmt.Printf("エンジニアですか？:%t", isEngineer)
 }
