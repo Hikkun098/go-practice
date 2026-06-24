a1d4cc0e47b9357dc0e031911c6d0e85373db9c9
Hikkun098 <mhhkn098@gmail.com>
2026-06-24 23:58:14 +0900

    03_types: 型変換とSprintf

--- 03_types/types.go
+++ 03_types/types.go
@@ -7,9 +7,15 @@ func main() {
 	var level int = 100
 	var point float64 = 4.5
 	var isEngineer bool = true
+	total := float64(level) + point
 
-	fmt.Printf("メッセージ:%s", message)
-	fmt.Printf("レベル:%d", level)
-	fmt.Printf("ポイント:%f", point)
-	fmt.Printf("エンジニアですか？:%t", isEngineer)
+	fmt.Printf("メッセージ: %s\n", message)
+	fmt.Printf("レベル: %d\n", level)
+	fmt.Printf("ポイント: %f\n", point)
+	fmt.Printf("エンジニアですか？: %t\n", isEngineer)
+	fmt.Println(total)
+	fmt.Printf("float64に変換: %f\n", float64(level))
+	fmt.Printf("intに変換: %d\n", int(point))
+	result := fmt.Sprintf("レベル: %d", level)
+	fmt.Println(result)
 }
