a90fadbc5d7117944be522d959f69dbacd8a4847
Hikkun098 <mhhkn098@gmail.com>
2026-06-25 23:52:15 +0900

    04_if_switch: if文とSwitchの使い方について学習

--- 04_if_switch/if_switch.go
+++ 04_if_switch/if_switch.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello from", "if_switch")
+	const firstScore int = 80
+  const secondScore int = 90
+
+	if firstScore >= 90 {
+		fmt.Println("優秀")
+	} else if firstScore >= 60 {
+		fmt.Println("合格")
+	} else {
+		fmt.Println("不合格")
+	}
+
+	switch {
+  case secondScore >= 90:
+    fmt.Println("優秀")
+  case secondScore >= 60:
+    fmt.Println("合格")
+  default:
+    fmt.Println("不合格")
+  }
 }
