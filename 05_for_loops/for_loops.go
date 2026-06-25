0e4d847db39cb5c760c27ad1b2bd4b3fa2ffcd43
Hikkun098 <mhhkn098@gmail.com>
2026-06-26 00:35:02 +0900

    05_for_loops: ループ処理について学習

--- 05_for_loops/for_loops.go
+++ 05_for_loops/for_loops.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello from", "for_loops")
+	for i := 1; i <= 10; i++ {
+		fmt.Println("1に1を足すこれを10回行う:", i)
+	}
+
+	count := 0
+	for count < 20 {
+		fmt.Println(count)
+		count++
+	}
+
+	fruits := []string{"apple", "orange", "banana"}
+	for index, value := range fruits {
+		fmt.Printf("index: %d, value: %s\n", index, value)
+	}
 }
