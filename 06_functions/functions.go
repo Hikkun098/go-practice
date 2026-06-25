7ad4c7dac4a84d8a6a5c13ee457c3d3478e03144
Hikkun098 <mhhkn098@gmail.com>
2026-06-26 01:27:28 +0900

    06_functions: 関数の基本について学習

--- 06_functions/functions.go
+++ 06_functions/functions.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+func add(a int, b int) int {
+    return a + b
+}
+
+func divide(a int, b int) (int, int) {
+    quotient := a / b
+    remainder := a % b
+    return quotient, remainder
+}
+
 func main() {
-	fmt.Println("Hello from", "functions")
+    quotient, remainder := divide(10, 3)
+    fmt.Println(quotient, remainder)
+    greet("Alice")
+}
+
+func greet(name string) {
+    fmt.Printf("Hello, %s!\n", name)
 }
