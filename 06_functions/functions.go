28d61b0cddfc32e84a3ddb8c25425eba79b4b66a
Hikkun098 <mhhkn098@gmail.com>
2026-06-27 23:40:43 +0900

    06_functions: 関数addの呼び出しをmain関数に追加

--- 06_functions/functions.go
+++ 06_functions/functions.go
@@ -3,21 +3,23 @@ package main
 import "fmt"
 
 func add(a int, b int) int {
-    return a + b
+	return a + b
 }
 
 func divide(a int, b int) (int, int) {
-    quotient := a / b
-    remainder := a % b
-    return quotient, remainder
+	quotient := a / b
+	remainder := a % b
+	return quotient, remainder
 }
 
 func main() {
-    quotient, remainder := divide(10, 3)
-    fmt.Println(quotient, remainder)
-    greet("Alice")
+	sum := add(5, 3)
+  fmt.Println("合計:", sum)
+	quotient, remainder := divide(10, 3)
+	fmt.Println(quotient, remainder)
+	greet("Alice")
 }
 
 func greet(name string) {
-    fmt.Printf("Hello, %s!\n", name)
+	fmt.Printf("Hello, %s!\n", name)
 }
