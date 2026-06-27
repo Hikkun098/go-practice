f102fe6684c28789b49aa53cb0f176007436414f
Hikkun098 <mhhkn098@gmail.com>
2026-06-27 23:56:50 +0900

    07_slices: スライスについて学習

--- 07_slices/slices.go
+++ 07_slices/slices.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello from", "slices")
+	numbers := []int{10, 20, 30, 40, 50}
+	fmt.Println(numbers)
+	numbers = append(numbers, 60)
+	fmt.Println(numbers)
+	fmt.Println(numbers[1:3])
+	fmt.Println(len(numbers))
+	fmt.Println(cap(numbers))
 }
