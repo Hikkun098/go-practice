b58578752922f07a927694aaf4df3314bf6a42c2
Hikkun098 <mhhkn098@gmail.com>
2026-06-28 00:09:43 +0900

    08_maps: マップについて学習

--- 08_maps/maps.go
+++ 08_maps/maps.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello from", "maps")
+	profile := map[string]string{
+		"name": "Shuu",
+		"age":  "30",
+		"job":  "Engineer",
+		"city": "Tokyo",
+	}
+	fmt.Println(profile)
+	fmt.Println(profile["name"])
+	fmt.Println(profile["age"])
+	fmt.Println(profile["job"])
+	fmt.Println(profile["city"])
+	value, ok := profile["name"]
+	fmt.Println(value, ok)
+	profile["alias"] = "Shuu-chan"
+	fmt.Println(profile["alias"])
+	profile["age"] = "31"
+	fmt.Println(profile["age"])
+	delete(profile, "job")
+	fmt.Println(profile)
 }
