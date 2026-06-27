538c296b22ce649a4323fa69f5d896982ac927fc
Hikkun098 <mhhkn098@gmail.com>
2026-06-28 02:19:38 +0900

    10_pointers: ポインタの学習 まずは挙動を確認してみる

--- 10_pointers/pointers.go
+++ 10_pointers/pointers.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+func double (n int) {
+	n = n * 2
+	fmt.Println("関数の中:", n)
+}
+
+func doublePointer (n *int) {
+	*n = *n * 2
+	fmt.Println("関数の中:", *n)
+}
+
 func main() {
-	fmt.Println("Hello from", "pointers")
+	num := 10
+  double(num)
+	doublePointer(&num)
+	fmt.Println("関数の外:", num)
 }
