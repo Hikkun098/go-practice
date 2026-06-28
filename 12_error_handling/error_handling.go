dfb55c0ab90f2653b82dfeb076d4055ea2d2da8c
Hikkun098 <mhhkn098@gmail.com>
2026-06-29 00:31:39 +0900

    12_error_handling: エラーハンドリングの基礎について学習

--- 12_error_handling/error_handling.go
+++ 12_error_handling/error_handling.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+    "errors"
+    "fmt"
+)
+
+func divide(a, b float64) (float64, error) {
+    // bが0ならエラーを返す
+    // ヒント: errors.New("エラーメッセージ")
+    // 正常なら計算結果とnilを返す
+		if b == 0 {
+			return 0, errors.New("0で割ることはできません。")
+		}
+		return a / b, nil
+}
 
 func main() {
-	fmt.Println("Hello from", "error_handling")
+	firstResult, err := divide(10, 3)
+	if err != nil {
+		fmt.Println("エラー:", err)
+	} else {
+		fmt.Println("結果:", firstResult)
+	}
+
+	secondResult, err2 := divide(10, 0)
+	if err2 != nil {
+		fmt.Println("エラー:", err2)
+	} else {
+		fmt.Println("結果:", secondResult)
+	}
 }
