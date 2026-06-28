54175033fdf7dffc26f795a3377edea8a15edd67
Hikkun098 <mhhkn098@gmail.com>
2026-06-28 22:53:03 +0900

    11_interfaces: インターフェース インターフェースを使って共通処理を作ってみる

--- 11_interfaces/interfaces.go
+++ 11_interfaces/interfaces.go
@@ -2,6 +2,34 @@ package main
 
 import "fmt"
 
+type Greeter interface {
+	greet() string
+}
+
+type Japanese struct {
+    name string
+}
+
+type American struct {
+    name string
+}
+
+func (j Japanese) greet() string {
+    return fmt.Sprintf("こんにちは、私は%sです", j.name)
+}
+
+func (a American) greet() string {
+    return fmt.Sprintf("Hello, I'm %s", a.name)
+}
+
+func sayHello(g Greeter) {
+	fmt.Println(g.greet())
+}
+
 func main() {
-	fmt.Println("Hello from", "interfaces")
+	j := Japanese{name: "Taro"}
+	a := American{name: "John"}
+
+	sayHello(j)
+	sayHello(a)
 }
